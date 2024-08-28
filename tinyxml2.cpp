6a3466b3cf3724bf7c516ca1bdef9150021a2cd4
Wei Hu <whuae@connect.ust.hk>
2024-08-28 15:34:09 +0800

    add TwinCAT precompiled header

--- tinyxml2.cpp
+++ tinyxml2.cpp
@@ -20,6 +20,11 @@ must not be misrepresented as being the original software.
 3. This notice may not be removed or altered from any source
 distribution.
 */
+#ifdef TC_VER
+#include "TcPch.h"
+#endif
+
+#pragma hdrstop
 
 #include "tinyxml2.h"
 
